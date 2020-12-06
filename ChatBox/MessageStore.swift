de3370de930b5cfe1b7225508c4befae34269c27
刘菁楷 <walker_walker@foxmail.com>
2020-12-06 16:49:42 +0800

    添加了保存Json文件的功能

--- ChatBox/MessageStore.swift
+++ ChatBox/MessageStore.swift
@@ -16,6 +16,33 @@ class MessageStore: NSObject {
     func addNewMsg(_ newMessage: Message) {
         messages.append(newMessage)
         addedMsg!(newMessage)
+        saveAsJsonFile()
+    }
+    
+    // 保存为Json文件
+    func saveAsJsonFile() {
+        
+        let dic = ["messages": messages]
+        let path = "/Users/macbookpro/Desktop/data.json"
+        do {
+            let enc = JSONEncoder()
+            enc.outputFormatting = .prettyPrinted
+            let data = try enc.encode(dic)
+            // 若文件存在则删掉新建
+            let fm = FileManager()
+            if fm.fileExists(atPath: path) {
+                try fm.removeItem(atPath: path)
+                fm.createFile(atPath: path, contents: nil, attributes: nil)
+            } else {
+                fm.createFile(atPath: path, contents: nil, attributes: nil)
+            }
+            if let fh = FileHandle(forWritingAtPath: path) {
+                fh.write(data)
+                try fh.close()
+            }
+        } catch {
+            print("!!!!!!!!")
+        }
     }
     
 }
